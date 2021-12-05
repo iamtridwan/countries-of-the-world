d9c839ebe0c7d3c132d384396343197601b00f3d
Tijani Ridwan <iamtridwan@gmail.com>
2021-12-05 03:17:36 +0100

    changes

--- src/app/modules/convert-string.pipe.ts
+++ src/app/modules/convert-string.pipe.ts
@@ -5,8 +5,19 @@ import { Pipe, PipeTransform } from '@angular/core';
 })
 export class ConvertStringPipe implements PipeTransform {
 
-  transform(value: string, character: string): string{
-    return value.replace(character, ', ');
+  transform(value: number): string{
+    let strVal = String(value)
+    let res: string = '';
+    for(let i = 0; i < strVal.length; i++){
+      if(i % 3 === 0){
+        res += strVal[i] + ', ';  
+      }
+      else{
+        res += strVal[i]
+      }
+    }
+    return res
+    
   }
 
 }
